abf5a79f5d303e421cfba97aa193d231d2139cb8
程征波 <wb-czb515173@alibaba-inc.com>
2019-03-28 16:39:28 +0800

    redis properties

--- src/main/java/com/example/java/spring/controller/UserController.java
+++ src/main/java/com/example/java/spring/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.java.spring.annotation.Mylogger;
 import com.example.java.spring.buildFactory.BeanContent;
 import com.example.java.spring.buildFactory.UseContentBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ public class UserController implements BeanContent {
     UseContentBean useContentBean;
 
     @Mylogger(name = "text")
+    @Cacheable
     @RequestMapping("/search")
     public String text() {
         return useContentBean.ascService.checkRAM() + " === \n"
